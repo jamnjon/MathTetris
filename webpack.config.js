dff1b1ae93cdc881e9ead9407cc7d6b0222fc88e
jamnjon <Jon@Jons-MacBook-Pro.local>
2016-05-16 15:02:36 -0700

    updating webpack issue

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  entry: "./li/math_tetris.js",
+  entry: "./lib/math_tetris.js",
   output: {
-  	filename: "./li/bundle.js"
+  	filename: "./lib/bundle.js"
   },
   devtool: 'source-map',
 };
